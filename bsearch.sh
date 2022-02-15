0a451342b384a4dfb2835b679ad18ddbb3caabbc
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2022-02-15 14:07:40 +0530

    feat: add firefox bookmarks

--- bsearch.sh
+++ bsearch.sh
@@ -9,12 +9,27 @@ copy_chrome_bookmarks() {
     if [ ! -z "$is_darwin" ]; then
         cp "$HOME/Library/Application Support/Google/Chrome/Default/Bookmarks" tmp/
     elif [ ! -z "$is_linux" ]; then
-        echo "linux"
         cp "$HOME/.config/chromium/Default/Bookmarks" tmp/
     fi
 }
 
 
-copy_chrome_bookmarks
+copy_firefox_bookmarks() {
+    if [ ! -z "$is_linux" ]; then
+        x=$(find ~/.mozilla/firefox/ -type f -name places.sqlite)
 
-python3 -m py "$@"
+        if [ -z "$x" ]; then
+            echo "Firefox no bookmarks"
+            return
+        fi
+
+        profile=$(cat ~/.mozilla//firefox/profiles.ini | grep -B2 Locked=1 | tail -n2 | grep Default | cut -d= -f2)
+
+        cp "$HOME/.mozilla/firefox/$profile/places.sqlite" tmp/
+    fi
+}
+
+copy_chrome_bookmarks
+copy_firefox_bookmarks
+
+python3 -m py "$@" 
